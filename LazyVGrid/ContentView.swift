96a78a98a19a9b8e606a12907ec30742383289ce
sudoswift <sudoswift@gmail.com>
2021-05-30 00:53:51 +0900

    Initial Commit

---
+++ LazyVGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LazyVGrid
+//
+//  Created by izvern on 2021/05/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LazyVGrid/LazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridApp.swift
+//  LazyVGrid
+//
+//  Created by izvern on 2021/05/30.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
