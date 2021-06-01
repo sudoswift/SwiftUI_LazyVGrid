cc3e838a117addc66eb4036064ce99d22dbcf419
sudoswift <sudoswift@gmail.com>
2021-06-01 23:52:53 +0900

    add LazyVStack

--- LazyVGrid/ContentView.swift
+++ LazyVGrid/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LazyVStack {
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
     }
 }
 
